640b60310a47a869725bee6102981f9a8c388721
NT Puujee <purevjargal@nashatech.com>
2025-09-28 03:36:00 +0800

    refactor: replace warning icon with image in NotFound component for improved visual representation

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -76,5 +76,3 @@ export async function POST(request: NextRequest) {
     error: null,
   });
 }
-
-
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,17 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import * as React from 'react';
-import { RiAlarmWarningFill } from 'react-icons/ri';
 
 export default function NotFound() {
   return (
     <main className='min-h-screen bg-gradient-to-br from-gray-50 to-gray-100'>
       <section className='bg-white'>
         <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
-          <RiAlarmWarningFill
-            size={60}
-            className='drop-shadow-glow animate-flicker text-red-500'
+          <Image
+            src='/images/not-found.png'
+            alt='404'
+            width={500}
+            height={500}
           />
           <h1 className='mt-8 text-4xl md:text-6xl'>Хуудас олдсонгүй</h1>
           <Link
